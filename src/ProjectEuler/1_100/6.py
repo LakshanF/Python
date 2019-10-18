f1dfab5e78c61f2a8a3295b9114a088446fe6c9f
Lakshan Fernando <lakshanf@microsoft.com>
2019-10-18 14:27:08 -0700

    6 solution

--- src/ProjectEuler/1_100/6.py
+++ src/ProjectEuler/1_100/6.py
@@ -9,3 +9,23 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+# sum square difference 25164150
+# --- 0.0020322799682617188 seconds ---
+
+import time
+start_time = time.time()
+
+sumOfSquares=0
+sum=0
+
+
+for num in range(1, 101):
+    sumOfSquares += num*num
+    sum += num
+    #print(num)
+
+diff = sum*sum - sumOfSquares
+
+print("Sum square difference %d" %(diff))
+print("--- %s seconds ---" % (time.time() - start_time))

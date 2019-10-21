4eed6ea22a2000288cf9d6f9e72025bfac3f8fb1
Lakshan Fernando <lakshanf@microsoft.com>
2019-10-21 15:26:38 -0700

    10 solution

--- src/ProjectEuler/1_100/10.py
+++ src/ProjectEuler/1_100/10.py
@@ -4,9 +4,25 @@
 # 
 # Find the sum of all the primes below two million.
 # 
-# 142913828922
+# Sum of all primes under 2M is 142913828922
+# --- 1548.936164855957 seconds ---
 
 import time
 start_time = time.time()
 
+primes = [2, 3, 5, 7, 11, 13]
+
+sumOfPrimes=sum(primes)
+
+for num in range(14, 2000001):
+    primeFound=True
+    for prime in primes:
+        if num%prime==0:
+            primeFound=False
+            break
+    if primeFound:
+        primes.append(num)
+        sumOfPrimes += num
+
+print('Sum of all primes under 2M is %d' %(sumOfPrimes))
 print('--- %s seconds ---' % (time.time() - start_time))

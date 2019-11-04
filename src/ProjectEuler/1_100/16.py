861b11ff049ca8134342b874f433199ffa5a24f3
Lakshan Fernando <lakshanf@microsoft.com>
2019-11-04 14:29:42 -0800

    16 solution

--- src/ProjectEuler/1_100/16.py
+++ src/ProjectEuler/1_100/16.py
@@ -1,13 +1,27 @@
 # Power digit sum
 # 
-# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 #
-# What is the sum of the digits of the number 21000?
+# What is the sum of the digits of the number 2^1000?
 # 
-# 1366
+# The sum is 1366
+# --- 0.0009722709655761719 seconds ---
 
+import math
 import time
 start_time = time.time()
 
+num=1
+
+for pow in range(1, 1001):
+    num += num
+
+sum=0
+origNum=num
+while num>0:
+    sum += num%10
+    num //= 10
+
+print('The sum is %d' %(sum))
 
 print('--- %s seconds ---' % (time.time() - start_time))
